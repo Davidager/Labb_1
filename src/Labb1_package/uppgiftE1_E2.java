3f1a5cc465435f187eeab8556854c0f596847a30
Davidager <david.malmstrm@gmail.com>
2016-11-02 16:04:45 +0100

    tog bort löjligt namn 123

--- src/Labb1_package/uppgiftE1_E2.java
+++ src/Labb1_package/uppgiftE1_E2.java
@@ -11,7 +11,7 @@ public class uppgiftE1_E2 {
         System.out.println(human1.getAge());
         System.out.println(human1.getName());
 
-        Human putte = new Human(25,"Putte123");
+        Human putte = new Human(25,"Putte");
         System.out.println(putte);
     }
 }
