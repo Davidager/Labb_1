67ca07b53eb26addaccb9e920d3395b36e51cc2a
Davidager <david.malmstrm@gmail.com>
2016-11-02 19:09:54 +0100

    E3 klart

--- src/Labb1_package/Human.java
+++ src/Labb1_package/Human.java
@@ -6,6 +6,10 @@ package Labb1_package;
 public class Human {
     private int age;
     private String name;
+    private static String[] nameArray = {"Wilma", "William", "Maja", "Lucas", "Ella", "Elias", "Emma", "Oscar", "Julia",
+        "Hugo", "Alice", "Viktor", "Alva", "Filip", "Linnea", "Erik", "Ida", "Emil", "Ebba", "Isak", "Elin", "Anton"};
+    private static int randomAge;
+    private static String randomName; // = nameArray[(int)(Math.random()*nameArray.length)];
 
     public String toString() {
         return "namn: " + name + ", ålder: " + String.valueOf(age);
@@ -19,10 +23,14 @@ public class Human {
         return name;
     }
 
-    public Human(int the_age, String the_name) {
-        age = the_age;
-        name = the_name;
+    public Human(int age, String name) {
+        this.age = age;
+        this.name = name;
     }
 
-    //public Human()
+    public Human() {
+        this(randomAge = (int)(Math.random() * 100), randomName = nameArray[(int)(Math.random()*nameArray.length)]);
+
+
+    }
 }
