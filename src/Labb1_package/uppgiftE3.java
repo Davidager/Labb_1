11b48225aa5211ccde99894e6024479093c12b82
Davidager <david.malmstrm@gmail.com>
2016-11-03 19:48:29 +0100

    E3 helt klart!

--- src/Labb1_package/uppgiftE3.java
+++ src/Labb1_package/uppgiftE3.java
@@ -1,12 +1,17 @@
 package Labb1_package;
 
+import java.util.Arrays;
+
 /**
  * Created by David on 02-Nov-16.
  */
 public class uppgiftE3 {
 
     public static void main(String[] args) {
-        Human human2 = new Human();
-        System.out.println(human2);
+        Human[] humanArray = new Human[15];
+        for(int i=0; i<15; i++) {
+            humanArray[i] = new Human();
+        }
+        System.out.println(Arrays.toString(humanArray));
     }
 }
