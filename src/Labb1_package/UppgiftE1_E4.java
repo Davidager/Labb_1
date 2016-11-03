75a4e51c38c03bc16f2464053f44306333199491
Bergsdal <bergsdal@kth.se>
2016-11-03 12:56:52 +0100

    Add files via upload

---
+++ src/Labb1_package/UppgiftE1_E4.java
@@ -0,0 +1,12 @@
+package Labb1_package;
+
+/**
+ * Created by Dexter on 2016-11-02.
+ */
+public class UppgiftE1_E4 {
+    public static void main(String[] args) {
+        Fysiker fys1 = new Fysiker();
+        System.out.println(fys1.getAge());
+        System.out.println(fys1.getYear());
+    }
+}
