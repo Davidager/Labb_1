396231c42d160a0c5da2f6c876a41e28938a5de4
Davidager <david.malmstrm@gmail.com>
2016-11-03 18:37:58 +0100

    E3 klart

---
+++ src/Labb1_package/uppgiftE3.java
@@ -0,0 +1,12 @@
+package Labb1_package;
+
+/**
+ * Created by David on 02-Nov-16.
+ */
+public class uppgiftE3 {
+
+    public static void main(String[] args) {
+        Human human2 = new Human();
+        System.out.println(human2);
+    }
+}
